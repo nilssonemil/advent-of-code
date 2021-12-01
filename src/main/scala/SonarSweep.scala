ff0b694273bcb18c508be33eeb2222766f09d4ba
Emil Nilsson <emil.nilsson@protonmail.com>
2021-12-01 22:31:22 +0100

    Sonar sweep

--- src/main/scala/SonarSweep.scala
+++ src/main/scala/SonarSweep.scala
@@ -1,5 +1,24 @@
-object SonarSweep extends App {
-  def cube(x: Int) = {
-    x * x * x
+import scala.io.Source
+
+object SonarSweep {
+  def measurementsLargerThanPredecessor(measurements: List[Int]) = {
+    measurements.map(_.toInt).sliding(2).filter(lastIsGreater).toList.size
+  }
+
+  def lastIsGreater(measurements: List[Int]) = {
+    measurements.last > measurements.head
+  }
+  
+  def threeMeasurementSlidingWindowSum(measurements: List[String]) = {
+    measurements.map(_.toInt).sliding(3).map(_.sum).toList
+  }
+  
+  def slidingWindowMeasurementsLargerThanPredecessor(measurements: List[String]) = {
+    measurementsLargerThanPredecessor(threeMeasurementSlidingWindowSum(measurements))
+  }
+
+  def main(args: Array[String]): Unit = {
+    val sweeps: List[String] = Source.fromResource("sonar-sweep.input").getLines.toList
+    println(slidingWindowMeasurementsLargerThanPredecessor(sweeps))
   }
 }
--- src/test/scala/SonarSweepTest.scala
+++ src/test/scala/SonarSweepTest.scala
@@ -1,5 +1,27 @@
+import scala.io.Source
+
 class SonarSweepTest extends org.scalatest.funsuite.AnyFunSuite {
-  test("CubeCalculator.cube") {
-    assert(SonarSweep.cube(3) === 27)
+  test("Read test input to list") {
+    val actual: List[String] = Source.fromResource("sonar-sweep.input").getLines.toList
+    val expected = List("199", "200", "208", "210", "200", "207", "240", "269", "260", "263")
+    assert(expected.equals(actual))
+  }
+
+  test("Test data has 7 measurements larger than previous") {
+    val sweeps: List[String] = Source.fromResource("sonar-sweep.input").getLines.toList
+    
+    assert(SonarSweep.measurementsLargerThanPredecessor(sweeps.map(_.toInt)) == 7)
+  }
+
+  test("measurement windows") {
+    val sweeps: List[String] = Source.fromResource("sonar-sweep.input").getLines.toList
+    val expected = List(607, 618, 618, 617, 647, 716, 769, 792)
+    val actual = SonarSweep.threeMeasurementSlidingWindowSum(sweeps)
+    assert(expected.equals(actual))
+  }
+
+  test("Test data has 5 measurement window increases") {
+    val sweeps: List[String] = Source.fromResource("sonar-sweep.input").getLines.toList
+    assert(5 == SonarSweep.slidingWindowMeasurementsLargerThanPredecessor(sweeps))
   }
 }
